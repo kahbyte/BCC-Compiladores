9a26ea906936e7e33443a5c5e6091421e6319f8e
kahbyte <kahbyte@gmail.com>
2023-04-24 14:57:35 -0300

    feat: finished lexer01

---
+++ lexer01/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+void Operador(char str[]){
+  char c;
+  int posString = 0;
+  char *verifica[9999];
+  int qtdCaractInd = 0;
+  char caractInd[9999][99];
+  for(int i = 0; i < strlen(str); i++){
+    c = str[i];
+    if (c == '+') {
+      verifica[posString] = "Soma\n";
+      posString++;
+    } 
+    else if(c == '-'){
+      verifica[posString]= "Subtração\n";
+      posString++;
+    }
+    else if(c == '*'){
+      verifica[posString]= "Multiplicação\n";
+      posString++;
+    }
+    else if(c == '/'){
+      verifica[posString]= "Divisão\n";
+      posString++;
+    }
+    else if(c ==' '){
+      verifica[posString] = "espaco\n";
+      posString++;
+    }      
+    else {
+      sprintf(caractInd[qtdCaractInd], "indeterminado(%c)\n", c);
+      verifica[posString] = caractInd[qtdCaractInd];
+      posString++;
+      qtdCaractInd++;
+    }
+  }
+  printf("Operadores: \n");
+  for (int i = 0; i < posString; i++) {
+    printf("%s", verifica[i]);
+  }
+}
+
+int main(int argc,char *argv[]) {
+  FILE *arq;
+  char c;
+  char code[9999];
+  int codeIndex = 0;
+  arq = fopen(argv[1], "r");
+  while(c!= EOF){
+    c = fgetc(arq);
+    code[codeIndex] = c;
+    codeIndex++;
+  }
+  code[codeIndex-1] = NULL;
+  fclose(arq);
+  Operador(code);
+  return 0;
+}
